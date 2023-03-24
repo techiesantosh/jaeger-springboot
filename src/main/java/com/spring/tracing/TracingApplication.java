d8a2d79c93f21ec2840fff1784ce951c7a2f13e0
Santosh Chougule <san007chougule@gmail.com>
2023-03-24 04:47:08 -0700

    Spring boot jaeger integration

---
+++ src/main/java/com/spring/tracing/TracingApplication.java
@@ -0,0 +1,22 @@
+package com.spring.tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.
+				build();
+	}
+}
